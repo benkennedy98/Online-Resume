29d964559de082412fb3712a5ea9acf01bcf7f37
benkennedy98 <bmhk2011@gmail.com>
2024-02-16 17:06:21 -0700

    Adding login logout button

--- Client/src/components/NavBar.jsx
+++ Client/src/components/NavBar.jsx
@@ -1,15 +1,29 @@
 import { Link } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
 import './NavBar.css';
 
 export const NavBar = () => {
+    const { isAuthenticated, loginWithRedirect, logout } = useAuth0()
     return (
         <div className='nav-bar'>
-            <nav className='links-container'>
-                <Link className='link' to='/'>Home</Link>
-                <Link className='link' to='/aboutme'>About Me</Link>
-                <Link className='link' to='/workhistory'>Work History</Link>
-                <Link className='link' to='/education'>Education</Link>
-            </nav>
+            {!isAuthenticated && (
+                <div className='auth-button'>
+                    <button onClick={() => loginWithRedirect({})}>Login</button>
+                </div>
+            )}
+            {isAuthenticated && (
+                <>
+                    <nav className='links-container'>
+                        <Link className='link' to='/'>Home</Link>
+                        <Link className='link' to='/aboutme'>About Me</Link>
+                        <Link className='link' to='/workhistory'>Work History</Link>
+                        <Link className='link' to='/education'>Education</Link>
+                    </nav>
+                    <div className='auth-button'>
+                        <button onClick={() => logout()}>logout</button>
+                    </div>
+                </>
+            )}
       </div>
     )
 }
