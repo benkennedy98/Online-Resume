702c679fc8c52330bff0d667a1a2bfe2aea19952
benkennedy98 <bmhk2011@gmail.com>
2024-02-20 12:55:36 -0700

    convert to ts and fixed formatting

---
+++ Client/src/components/App.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+import { NavBar } from './NavBar';
+import { Home } from '../pages/Home';
+import { AboutMe } from '../pages/AboutMe';
+import { WorkHistory } from '../pages/WorkHistory';
+import { Education } from '../pages/Education';
+import './App.css';
+
+const App = () => {
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NavBar />
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutme" element={isAuthenticated ? <AboutMe /> : <Navigate to="/" />} />
+        <Route
+          path="/workhistory"
+          element={isAuthenticated ? <WorkHistory /> : <Navigate to="/" />}
+        />
+        <Route path="/education" element={isAuthenticated ? <Education /> : <Navigate to="/" />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ Client/src/components/NavBar.tsx
@@ -0,0 +1,39 @@
+import { Link } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+import './NavBar.css';
+
+export const NavBar = () => {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+  return (
+    <div className="nav-bar">
+      {!isAuthenticated && (
+        <div className="auth-button">
+          <button className="log" onClick={() => loginWithRedirect({})}>
+            Login
+          </button>
+        </div>
+      )}
+      {isAuthenticated && (
+        <>
+          <nav className="links-container">
+            <Link className="link" to="/">
+              Home
+            </Link>
+            <Link className="link" to="/aboutme">
+              About Me
+            </Link>
+            <Link className="link" to="/workhistory">
+              Work History
+            </Link>
+            <Link className="link" to="/education">
+              Education
+            </Link>
+            <button className="log" onClick={() => logout()}>
+              logout
+            </button>
+          </nav>
+        </>
+      )}
+    </div>
+  );
+};
