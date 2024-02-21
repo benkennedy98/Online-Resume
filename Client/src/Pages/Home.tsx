6b07ecb2d2f9b0a5c4f74a80a7d81ee5c481ca93
benkennedy98 <bmhk2011@gmail.com>
2024-02-21 11:40:19 -0700

    adding icons

--- Client/src/Pages/Home.tsx
+++ Client/src/Pages/Home.tsx
@@ -1,18 +1,27 @@
 import './Home.css';
-
 export const Home = () => {
+  const iconPath = process.env.PUBLIC_URL + '/icons/';
+
   return (
     <div className="page-container">
       <div className="title-container">
         <div className="title">Welcome!</div>
       </div>
+      <div className="icon-container">
+        <img className="icon" src={`${iconPath}react.svg`} alt="" />
+        <img className="icon" src={`${iconPath}typescript.svg`} alt="" />
+        <img className="icon" src={`${iconPath}auth0.svg`} alt="" />
+        <img className="icon" src={`${iconPath}css.svg`} alt="" />
+      </div>
       <div className="body-container">
         <div className="body">
           This React Typescript application is here to display my skills and knowledge as a
           developer. I am using a long list of technologies and packages to create the website you
-          see before you. The fist being React, this project started out as a simple create react
-          app that I have been working on in my free time to keep my skills fresh and sharp. I am
-          using Typescript as it is a simple tool that helps prevent bugs and keeps project
+          see before you. The first being React, this project started out as a simple create react
+          app that I have been working on in my free time to keep my skills fresh and sharp.
+        </div>
+        <div className="body">
+          I am using Typescript as it is a simple tool that helps prevent bugs and keeps project
           development running smoothly. I am using a free auth tool called Auth 0 to handle
           authentication and it was a really simple process to get setup. I used a series on linked
           in learning to get me pointed in a good direction and then a series of stack overflow
