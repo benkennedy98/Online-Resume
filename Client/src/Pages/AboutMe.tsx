dd53ecf101509291ce699a9b692db6bfe6fa74d8
benkennedy98 <bmhk2011@gmail.com>
2024-02-27 18:03:08 -0700

    adding server call

--- Client/src/Pages/AboutMe.tsx
+++ Client/src/Pages/AboutMe.tsx
@@ -1,9 +1,24 @@
-export const AboutMe = () => {
-    return (
-        <div className='page-container'>
-            <div className='title-container'>
-                <div className='title'>About Me</div>
-            </div>
-        </div>
-    )
+import axios from 'axios';
+
+interface Response {
+  text: string;
 }
+
+export const AboutMe = () => {
+  axios
+    .get(`http://localhost:3001/aboutMe`)
+    .then(({ data }: { data: Response }) => {
+      console.log(data);
+    })
+    .catch((error: string) => {
+      console.error(error);
+    });
+
+  return (
+    <div className="page-container">
+      <div className="title-container">
+        <div className="title">About Me</div>
+      </div>
+    </div>
+  );
+};
--- Client/src/Pages/Home.tsx
+++ Client/src/Pages/Home.tsx
@@ -18,10 +18,8 @@ export const Home = () => {
           This React Typescript application is here to display my skills and knowledge as a
           developer. I am using a long list of technologies and packages to create the website you
           see before you. The first being React, this project started out as a simple create react
-          app that I have been working on in my free time to keep my skills fresh and sharp.
-        </div>
-        <div className="body">
-          I am using Typescript as it is a simple tool that helps prevent bugs and keeps project
+          app that I have been working on in my free time to keep my skills fresh and sharp. I am
+          using Typescript as it is a simple tool that helps prevent bugs and keeps project
           development running smoothly. I am using a free auth tool called Auth 0 to handle
           authentication and it was a really simple process to get setup. I used a series on linked
           in learning to get me pointed in a good direction and then a series of stack overflow
